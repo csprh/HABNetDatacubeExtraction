cfcde36b2e14d3777f4eef028e91d51f1c414eb1
Paul Hill <csprh@bc4login1.bc4.acrc.priv>
2018-05-23 16:16:44 +0100

    update bash script

--- testData2.sh
+++ testData2.sh
@@ -12,6 +12,7 @@ module load apps/matlab/2017a
 
 cd $SLURM_SUBMIT_DIR
 echo $SLURM_SUBMIT_DIR
-cd /mnt/storage/home/csprh/code/HAB/genHDFData
-matlab -nodisplay -nosplash -r getDataOuter > outfile.txt < /dev/null 
+cd /mnt/storage/home/csprh/code/HAB/extractData
+#matlab -nodisplay -nosplash -r getDataOuter > outfile.txt < /dev/null 
+matlab -nodisplay -nosplash -r getDataOuter
 
