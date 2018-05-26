879906fd251f29adbce004047b2d98c7e5b130fb
csprh <paul.hill@bristol.ac.uk>
2018-05-26 11:17:31 +0400

    Bug fix 1

--- getDataOuter.m
+++ getDataOuter.m
@@ -1,4 +1,8 @@
 function getDataOuter;
+%% Top level code that loads config, loads .mat ground truth file, 
+%  searches for all relevant .nc granules (using fd_matchup.py and NASA's
+%  CMR interface).  Datacubes are formed from all the local .nc granules
+
 clear; close all;
 
 mac = ismac;
@@ -9,6 +13,7 @@ else
     tmpStruct = xml2struct('configHAB.xml');
 end
 
+%% load all config from XML file
 confgData.inputFilename = tmpStruct.confgData.inputFilename.Text;
 confgData.wgetStringBase = tmpStruct.confgData.wgetStringBase.Text;
 confgData.outDir = tmpStruct.confgData.outDir.Text;
@@ -19,15 +24,17 @@ confgData.numberOfSamples = str2double(tmpStruct.confgData.numberOfSamples.Text)
 
 load(confgData.inputFilename);
 if confgData.numberOfSamples == -1;   confgData.numberOfSamples = length(count2); end;
+
+%% Loop through all samples in .mat Ground Truth File
 for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
     try
         thisLat = latitude(ii);
         thisLon = longitude(ii);
         thisCount = count2(ii);
-        zoneHrDiff = timezone(thisLon); % Try to limit the end date to 11pm on the day of capture (local time).
-        endTimeUTC = 23+zoneHrDiff;
+        zoneHrDiff = timezone(thisLon); 
+        endTimeUTC = 12+zoneHrDiff; % Assume that the sample is taken at midday
         dayEnd = sample_date(ii);
-        if (endTimeUTC) > 24; endTimeUTC = endTimeUTC - 24; dayEnd = dayEnd + 1; end;
+        dayEndFraction = dayEnd+endTimeUTC/24;
         dayStart = dayEnd - confgData.numberOfDaysInPast;   
         dayStartS = datestr(dayStart,29);
         dayEndS = datestr(dayEnd,29);
@@ -37,17 +44,17 @@ for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
         exeName = ['python  fd_matchup.py --data_type=oc --sat=modisa ' '--slat=' num2str(thisLat) ' --slon=' num2str(thisLon) ' --stime=' dayStartS UTCTime ' --etime=' dayEndS UTCTime];
         system(exeName);
         
-        % Extract OC lines from output
+        % Loop through .nc files, veryify and download / extract
         fid = fopen('Output.txt');   tline = fgetl(fid);
         indInput = 1; clear OCLines; clear thisInput; clear thisList;
         while ischar(tline)
-            [filepath,name,ext] = fileparts(tline);
-            thisDate=julian2time(name(2:14));
+            [filepath,thisName,ext] = fileparts(tline);
+            thisDate=julian2time(thisName(2:14));
             endOfLine = tline(end-4:end);
             if strcmp(endOfLine,'OC.nc')
                 thisInput{indInput}.line = tline;
                 thisInput{indInput}.date = thisDate;
-                thisInput{indInput}.deltadate = thisDate-inputDay;
+                thisInput{indInput}.deltadate = thisDate-dayEndFraction;
                 indInput = indInput + 1;
             end
             tline = fgetl(fid);
@@ -61,6 +68,7 @@ for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
         closeest = find(min(abs(thisList))==abs(thisList));
         closeestIndex = find(sortIndex==closeest);
         historicalIndex = fliplr(sortIndex(1:closeestIndex));
+        %% Loop through previous times and extract images and points from .nc files
         % iyyyydddhhmmss.L2_rrr_ppp,
         % where i is the instrument identifier  yyyydddhhmmss
         clear theseDates theseDeltaDates theseImages;
@@ -80,7 +88,7 @@ for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
             theseDeltaDates{iii} = thisDeltaDate;
             thesePointsNew = [thesePoints ones(size(thesePoints,1),1)*thisDeltaDate];
             thesePointsOutput = [thesePointsOutput; thesePointsNew];
-            %Put image and date into structure
+            % Delete the .nc file
             wdelString = 'rm *.nc';
             unix(wdelString);
         end
@@ -88,6 +96,7 @@ for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
         thisName = num2str(ii);
         h5name = [confgData.outDir 'flor' thisName '.h5']
         
+        %Put images, count, dates and deltadates into output .H5 file
         hdf5write(h5name,['/thisCount'],thisCount);
         hdf5write(h5name,['/inputDay'],inputDay, 'WriteMode','append');
         hdf5write(h5name,['/' thisVar '/Ims'],theseImages, 'WriteMode','append');
@@ -101,8 +110,11 @@ for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
     end
 end
 
+%% julian2time takes the julian day of the year contained in the .nc granule
+%  and converts it to integer datenum (as output by datestr).
 function t=julian2time(str)
 % convert NASA yyyydddHHMMSS to datenum
+% ddd starts with 1 (therefore we have to take 1 away
 ddd=str2double(str(5:7));
 jan1=[str(1:4),'0101',str(8:13)];  % day 1
 t=datenum(jan1,'yyyymmddHHMMSS')+ddd-1;
