dbb7d3c1962fc21dafa38559752551352844d98d
csprh <paul.hill@bristol.ac.uk>
2018-06-01 15:07:16 +0100

    Final Commit Before Move Centre of GetDataOuter code into a function

--- getDataOuter.m
+++ getDataOuter.m
@@ -1,5 +1,5 @@
 function getDataOuter;
-%% Top level code that loads config, loads .mat ground truth file, 
+%% Top level code that loads config, loads .mat ground truth file,
 %  searches for all relevant .nc granules (using fd_matchup.py and NASA's
 %  CMR interface).  Datacubes are formed from all the local .nc granules
 
@@ -21,6 +21,8 @@ confgData.distance1 = str2double(tmpStruct.confgData.distance1.Text);
 confgData.resolution = str2double(tmpStruct.confgData.resolution.Text);
 confgData.numberOfDaysInPast = str2double(tmpStruct.confgData.numberOfDaysInPast.Text);
 confgData.numberOfSamples = str2double(tmpStruct.confgData.numberOfSamples.Text);
+confgData.mods = tmpStruct.confgData.Modality;
+numberOfMods = length(confgData.mods);
 
 load(confgData.inputFilename);
 if confgData.numberOfSamples == -1;   confgData.numberOfSamples = length(count2); end;
@@ -31,82 +33,85 @@ for ii = 1: confgData.numberOfSamples %Loop through all the ground truth entries
         thisLat = latitude(ii);
         thisLon = longitude(ii);
         thisCount = count2(ii);
-        zoneHrDiff = timezone(thisLon); 
-        endTimeUTC = 23+zoneHrDiff; % Assume that the sample is taken at 11pm
+        zoneHrDiff = timezone(thisLon);
+        % Adjust input time / date: Assume that the sample is taken at 11pm
+        endTimeUTC = 23+zoneHrDiff;
         dayEnd = sample_date(ii);
         if endTimeUTC > 24; endTimeUTC = endTimeUTC-24; dayEnd=dayEnd+1; end;
         dayEndFraction = dayEnd+endTimeUTC/24;
-        dayStart = dayEnd - confgData.numberOfDaysInPast;   
+        dayStart = dayEnd - confgData.numberOfDaysInPast;
         dayStartS = datestr(dayStart,29);
         dayEndS = datestr(dayEnd,29);
         UTCTime = sprintf('T%02d:00:00Z', endTimeUTC);
         
-        % Search for "granules" at a particular lat, long and date range (output goes in Output.txt)
-        exeName = ['python  fd_matchup.py --data_type=oc --sat=modisa ' '--slat=' num2str(thisLat) ' --slon=' num2str(thisLon) ' --stime=' dayStartS UTCTime ' --etime=' dayEndS UTCTime];
-        system(exeName);
-        
-        % Loop through .nc files, veryify and download / extract
-        fid = fopen('Output.txt');   tline = fgetl(fid);
-        indInput = 1; clear OCLines; clear thisInput; clear thisList;
-        while ischar(tline)
-            [filepath,thisName,ext] = fileparts(tline);
-            thisDate=julian2time(thisName(2:14));
-            endOfLine = tline(end-4:end);
-            if strcmp(endOfLine,'OC.nc')
-                thisInput{indInput}.line = tline;
-                thisInput{indInput}.date = thisDate;
-                thisInput{indInput}.deltadate = dayEndFraction-thisDate;
-                indInput = indInput + 1;
+        for modIndex = 1:numberOfMods
+            thisMod = confgData.mods{modIndex}.Text;
+            subMods = strsplit(thisMod,'-');
+            
+            % Search for "granules" at a particular lat, long and date range (output goes in Output.txt)
+            exeName = ['python  fd_matchup.py --data_type=' subMods{1} ' --sat=' subMods{2} ' --slat=' num2str(thisLat) ' --slon=' num2str(thisLon) ' --stime=' dayStartS UTCTime ' --etime=' dayEndS UTCTime];
+            system(exeName);
+            
+            %% Loop through .nc files, veryify and download / extract
+            fid = fopen('Output.txt');   tline = fgetl(fid);
+            indInput = 1;  clear thisInput; clear thisList;
+            while ischar(tline)
+                [filepath,thisName,ext] = fileparts(tline);
+                thisDate=julian2time(thisName(2:14));
+                endOfLine = tline(end-4:end);
+                %if strcmp(endOfLine,'OC.nc')
+                    thisInput{indInput}.line = tline;
+                    thisInput{indInput}.date = thisDate;
+                    thisInput{indInput}.deltadate = dayEndFraction-thisDate;
+                    indInput = indInput + 1;
+                %end
+                tline = fgetl(fid);
             end
-            tline = fgetl(fid);
+            
+            %% Re-order the list of found data
+            for iii = 1: length(thisInput); thisList(iii) = thisInput{iii}.deltadate; end;
+            [sorted sortIndex] = sort(thisList);
+            closeest = find(max(abs(thisList))==abs(thisList));
+            closeestIndex = find(sortIndex==closeest);
+            historicalIndex = (sortIndex(1:closeestIndex));
+            
+            %% Loop through previous times and extract images and points from .nc files
+            % iyyyydddhhmmss.L2_rrr_ppp,
+            % where i is the instrument identifier  yyyydddhhmmss
+            clear theseDates theseDeltaDates theseImages;
+            thesePointsOutput = [];
+            for iii = 1:length(historicalIndex)
+                thisIndex = historicalIndex(iii);
+                thisLine = thisInput{thisIndex}.line;
+                thisDate = thisInput{thisIndex}.date;
+                thisDeltaDate = thisInput{thisIndex}.deltadate;
+                wgetString = [confgData.wgetStringBase ' ' thisLine];
+                unix(wgetString);
+                [filepath,name,ext] = fileparts(thisLine);
+                fileName = [name ext];
+                [theseImages{iii} thesePoints] = getData(fileName,  thisLat, thisLon, confgData.distance1, confgData.resolution, ['/geophysical_data/' subMods{3}]);
+                theseDates{iii} = thisDate;
+                theseDeltaDates{iii} = thisDeltaDate;
+                thesePointsNew = [thesePoints ones(size(thesePoints,1),1)*thisDeltaDate];
+                thesePointsOutput = [thesePointsOutput; thesePointsNew];
+                % Delete the .nc file
+                wdelString = 'rm *.nc';
+                unix(wdelString);
+            end
+            
+            thisName = num2str(ii);
+            h5name = [confgData.outDir 'flor' thisName '.h5'];
+            %Put images, count, dates and deltadates into output .H5 file
+            hdf5write(h5name,['/thisCount'],thisCount);
+            hdf5write(h5name,['/dayEndFraction'],dayEndFraction, 'WriteMode','append');
+            hdf5write(h5name,['/' thisMod  '/Ims'],theseImages, 'WriteMode','append');
+            hdf5write(h5name,['/' thisMod  '/theseDates'],theseDates, 'WriteMode','append');
+            hdf5write(h5name,['/' thisMod  '/theseDeltaDates'],theseDeltaDates, 'WriteMode','append');
+            hdf5write(h5name,['/' thisMod  '/thesePointsOutput'],thesePointsOutput, 'WriteMode','append');
+            
+            h5disp(h5name);
+            fclose(fid);
         end
-        
-        for iii = 1: length(thisInput)
-            thisList(iii) = thisInput{iii}.deltadate;
-        end
-        [sorted sortIndex] = sort(thisList);
-        
-        closeest = find(max(abs(thisList))==abs(thisList));
-        closeestIndex = find(sortIndex==closeest);
-        historicalIndex = (sortIndex(1:closeestIndex));
-        %% Loop through previous times and extract images and points from .nc files
-        % iyyyydddhhmmss.L2_rrr_ppp,
-        % where i is the instrument identifier  yyyydddhhmmss
-        clear theseDates theseDeltaDates theseImages;
-        thesePointsOutput = [];
-        for iii = 1:length(historicalIndex)
-            thisIndex = historicalIndex(iii);
-            thisLine = thisInput{thisIndex}.line;
-            thisDate = thisInput{thisIndex}.date;
-            thisDeltaDate = thisInput{thisIndex}.deltadate;
-            wgetString = [confgData.wgetStringBase ' ' thisLine];
-            unix(wgetString);
-            [filepath,name,ext] = fileparts(thisLine);
-            fileName = [name ext];
-            thisVar = 'chlor_a';
-            [theseImages{iii} thesePoints] = getData(fileName,  thisLat, thisLon, confgData.distance1, confgData.resolution, ['/geophysical_data/' thisVar]);
-            theseDates{iii} = thisDate;
-            theseDeltaDates{iii} = thisDeltaDate;
-            thesePointsNew = [thesePoints ones(size(thesePoints,1),1)*thisDeltaDate];
-            thesePointsOutput = [thesePointsOutput; thesePointsNew];
-            % Delete the .nc file
-            wdelString = 'rm *.nc';
-            unix(wdelString);
-        end
-        
-        thisName = num2str(ii);
-        h5name = [confgData.outDir 'flor' thisName '.h5']
-        
-        %Put images, count, dates and deltadates into output .H5 file
-        hdf5write(h5name,['/thisCount'],thisCount);
-        hdf5write(h5name,['/dayEndFraction'],dayEndFraction, 'WriteMode','append');
-        hdf5write(h5name,['/' thisVar '/Ims'],theseImages, 'WriteMode','append');
-        hdf5write(h5name,['/' thisVar '/theseDates'],theseDates, 'WriteMode','append');
-        hdf5write(h5name,['/' thisVar '/theseDeltaDates'],theseDeltaDates, 'WriteMode','append');
-        hdf5write(h5name,['/' thisVar '/thesePointsOutput'],thesePointsOutput, 'WriteMode','append');
-        
-        h5disp(h5name);
-        fclose(fid);
     catch
     end
 end
