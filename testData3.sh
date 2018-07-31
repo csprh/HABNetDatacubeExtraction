a34e83f1b6669e586199fa15922d45a463681ddc
csprh <paul.hill@bristol.ac.uk>
2018-07-31 19:07:04 +0400

    local update

---
+++ testData3.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cd /home/cosc/csprh/linux/HABCODE/code/HAB/extractData
+#matlab -nodisplay -nosplash -r getDataOuter > outfile.txt < /dev/null 
+matlab -nodisplay -nosplash -r getDataOuter
+
