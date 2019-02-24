5d12ae1a8e151e4b98ae93eac6dc68ee085faceb
csprh <paul.hill@bristol.ac.uk>
2019-02-24 23:21:49 +0000

    update

--- getDataOuter.m
+++ getDataOuter.m
@@ -329,10 +329,10 @@ function addToH5(h5name, thisMod, theseImages, theseDates, theseDeltaDates, thes
 end
 
 function logErr(e,str_iden)
-    fileID = fopen('errors.txt','at');
-    identifier = ['Error procesing sample ',str_iden, ' at ', datestr(now)];
-    text = [e.identifier, '::', e.message];
-    fprintf(fileID,'%s\n\r ',identifier);
-    fprintf(fileID,'%s\n\r ',text);
-    fclose(fileID);
+    %fileID = fopen('errors.txt','at');
+    %identifier = ['Error procesing sample ',str_iden, ' at ', datestr(now)];
+    %text = [e.identifier, '::', e.message];
+    %fprintf(fileID,'%s\n\r ',identifier);
+    %fprintf(fileID,'%s\n\r ',text);
+    %fclose(fileID);
 end
