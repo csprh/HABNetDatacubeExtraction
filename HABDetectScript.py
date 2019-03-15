b9528d63afa660240cb7bbe9d8b2986d9632293c
csprh <paul.hill@bristol.ac.uk>
2019-03-15 13:44:30 +0000

    update5

--- HABDetectScript.py
+++ HABDetectScript.py
@@ -25,8 +25,7 @@ h5name = '/home/cosc/csprh/linux/HABCODE/scratch/HAB/tmpTest/testCubes/Cube_0907
 outputDirectory = '/home/cosc/csprh/linux/HABCODE/scratch/HAB/tmpTest/CNNIms'
 os.chdir(r'../modelHAB')
 #eng.HABDetect(h5name, outputDirectory)
-os.system('HABDetect ' + ' '+ h5name + ' ' + outputDirectory)
-outputImagesFromDataCube(outputDirectory, numberOfDays, groupMinMax, inputRangeX, inputRangeY, alphaSize, outputRes, h5name)
+p = os.system('HABDetect ' + ' '+ h5name + ' ' + outputDirectory)
 extract_features('cnfgXMLs/NASNet11_lstm0.xml', outputDirectory)
 testHAB('cnfgXMLs/NASNet11_lstm0.xml', outputDirectory)
 
